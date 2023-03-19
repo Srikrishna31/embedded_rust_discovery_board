b09def04a5c390366bfa34d905f3db91a7f93569
Krishna Addepalli <coolkrishna31@gmail.com>
2023-03-19 12:15:29 +0100

    Implemented the LED Roulette

--- led_roulette/src/main.rs
+++ led_roulette/src/main.rs
@@ -14,23 +14,35 @@ use microbit::{
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let mut board = Board::take().unwrap();
+    let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
-    let light_it_all = [
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
+    let mut leds = [
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
     ];
 
+    let mut i = 0;
+    let mut j = 0;
     //infinite loop; just so we don't leave this stack frame
     loop {
         // Show light_it_all for 1000ms
-        display.show(&mut timer, light_it_all, 1000);
-        // clear the display again
-        display.clear();
-        timer.delay_ms(1000_u32);
+        display.show(&mut timer, leds, 50);
+        leds[i][j] = 0;
+
+        if i == 0 && j < 4 {
+            j += 1;
+        } else if j == 4 && i < 4 {
+            i += 1;
+        } else if i ==4 && j > 0 {
+            j -= 1;
+        } else if j == 0 && i > 0 {
+            i -= 1;
+        }
+
+        leds[i][j] = 1;
     }
 }
