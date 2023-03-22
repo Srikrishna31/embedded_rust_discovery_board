8598515f418c4ec7d73152819bfe838d54c2c6b7
Krishna Addepalli <coolkrishna31@gmail.com>
2023-03-22 16:40:57 +0100

    Implemented a simple echo server

--- uart/src/main.rs
+++ uart/src/main.rs
@@ -2,7 +2,8 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
+use rtt_target::{rprintln, rtt_init_print};
+use heapless::Vec;
 use panic_rtt_target as _;
 use core::fmt::Write;
 
@@ -53,8 +54,41 @@ fn main() -> ! {
 
     // nb is a "Minimal and reusable non-blocking I/O layer. It allows us to write code that can
     // conduct hardware operations in the background while we go and do other work (non-blocking).
-    write!(serial, "The quick brown fox jumps over the lazy dog.\r\n").unwrap();
-    nb::block!(serial.flush()).unwrap();
+    // write!(serial, "The quick brown fox jumps over the lazy dog.\r\n").unwrap();
+    // nb::block!(serial.flush()).unwrap();
 
-    loop {}
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8,32> = Vec::new();
+    loop {
+        buffer.clear();
+
+        // TODO: Receive a user request. Each user request ends with ENTER
+        // NOTE `buffer.push` returns a `Result`. handle the error by responding with an error message
+        loop {
+            let b = nb::block!(serial.read()).unwrap();
+
+            nb::block!(serial.write(b)).unwrap();
+
+            if b == 0x0D {
+                break;
+            }
+
+            match buffer.push(b)  {
+                Err(e) => rprintln!("Error pushing byte value into error: {}", e),
+                _ => {}
+            };
+        }
+        buffer.reverse();
+
+        write!(serial, "\r\n");
+        for b in buffer.iter() {
+            nb::block!(serial.write(*b)).unwrap();
+        }
+
+        // write!(serial, "{}", buffer as &str).unwrap();
+
+        write!(serial, "\r\n");
+
+        nb::block!(serial.flush()).unwrap();
+    }
 }
